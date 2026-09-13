5a5d928b641013dc26c438d3f9935e9044c07f75
Jousik <artemberov2001@mail.ru>
2026-09-13 11:38:36 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCWK1bFAd_MLWyQcaJi4Am_uU1W32XkKK0",
+  authDomain: "tutor-crm-d382c.firebaseapp.com",
+  projectId: "tutor-crm-d382c",
+  storageBucket: "tutor-crm-d382c.firebasestorage.app",
+  messagingSenderId: "718394042232",
+  appId: "1:718394042232:web:6f3644a2da080e55cbca8d"
+};
